c53d14a059e7adf6b6f3e2332eb59912feb09dbc
Marc <marctimothyronquillo@gmail.com>
2026-09-17 10:53:05 +0300

    added keymaps configuration

--- init.lua
+++ init.lua
@@ -4,26 +4,26 @@ vim.g.mapleader = " "
 -- 2. Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- 3. Load LazyVim Core & Python extra
 require("lazy").setup({
-  spec = {
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    { import = "lazyvim.plugins.extras.lang.python" },
-  },
-  defaults = { lazy = false, version = false },
-  checker = { enabled = true },
+	spec = {
+		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
+		{ import = "lazyvim.plugins.extras.lang.python" },
+	},
+	defaults = { lazy = false, version = false },
+	checker = { enabled = true },
 })
---
+++ lua/config/keymaps.lua
@@ -0,0 +1,3 @@
+-- Remapping arrows
+vim.keymap.set("n", "<Up>", "j")
+vim.keymap.set("n", "<Down>", "k")
